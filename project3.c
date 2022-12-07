ca232b131f0a415ece88e5b336b23d65ab8269c1
bCardenCode <brady.a.carden.-1@ou.edu>
2022-12-07 14:05:26 -0600

    Testing getting back to lines after switch

--- project3.c
+++ project3.c
@@ -552,7 +552,8 @@ int main(int argc, char** argv) {
     while(incompleteFiles > 0) {
         int instrsRun = 0;
         while(instrsRun < quantum) {
-    
+            
+            /*
             printf("here1\n");
             if(currentFile->data->lastAllocFailed == 1) {
                 printf("here2\n");
@@ -566,6 +567,7 @@ int main(int argc, char** argv) {
                     //currentFile->data->lastExecuted++;
                 }
             } 
+            */
 
             /*
             //Gets back to last executed line
